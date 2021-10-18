0393e41a529bd9bfe9e44686f03265e57cdf46bf
Igor Kulikov <kiaz2990@gmail.com>
2021-10-18 13:46:51 +0300

    add homework

---
+++ example1.py
@@ -0,0 +1,26 @@
+first = 10
+second = 30
+
+third = first + second
+print(third)
+
+third = first - second
+print(third)
+
+third = first * second
+print(third)
+
+third = first / second
+print(third)
+
+third += first
+print(third)
+
+third = second // first
+print(third)
+
+third = first % second
+print(third)
+
+third = 1 + first * second
+print(third)
