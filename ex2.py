6aae2204f84509760889089c0fb8e50b60fedb79
Igor Kulikov <kiaz2990@gmail.com>
2021-10-19 12:32:26 +0300

    add homework2

---
+++ ex2.py
@@ -0,0 +1,13 @@
+# 1 task
+value_1 = int(input("enter an integer :"))
+print(value_1 * 2)
+
+###########################################
+# 2 task
+value_2 = float(input("enter a floating point number :"))
+print(value_2 * 2)
+
+###########################################
+# 3 task
+my_string = input("enter the string :")
+print(my_string * 2)
